fb4e64a568563a39d26f31fba030123245c3e557
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2020-12-16 15:36:44 +0200

    __get__ to show correct repr() and str()

--- setenum/__init__.py
+++ setenum/__init__.py
@@ -10,13 +10,31 @@ class SetEnumMeta(EnumMeta):
         # TODO: class check is required
         return member._name_ in cls._member_map_
 
+
 class SetEnum(Enum, metaclass=SetEnumMeta):
-    pass
+    
+    # TODO: https://docs.python.org/3/reference/datamodel.html#customizing-instance-and-subclass-checks
+    # This is a somewhat ugly trick to make repr and print show on which 
+    
+    def __get__(self, obj, objtype=None):
+        self.__called_from = objtype
+        return self
+
+    def __repr__(self):
+        return "<%s.%s: %r>" % (
+                self.__called_from.__name__, self._name_, self._value_)
+
+    def __str__(self):
+        return "%s.%s" % (self.__called_from.__name__, self._name_)
+
+    
+
 
 
 def includes(cls_to_include):
     def inner(cls):
         for name, obj in cls_to_include._member_map_.items():
+            setattr(cls, name, obj)
             cls._member_map_[name] = obj
             cls._member_names_.append(name)
         return cls
