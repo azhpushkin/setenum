3b6446817bceb4311d41594868dbdcb3b7eded67
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2020-12-16 20:13:03 +0200

    parametrize to test both subset and superset

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,36 +1,62 @@
 import pytest
-from setenum import SetEnum, as_superset_of
+from setenum import SetEnum, as_superset_of, as_subset_of
 
 # Example SetEnums
 
-class PythonDependencies(SetEnum):
-    DJANGO = 'django'
-    FLASK = 'flask'
-    FASTAPI = 'fastapi'
+# Different ways to create the same Enum structure
+
+def create_via_superset():
+    class PythonDependencies(SetEnum):
+        DJANGO = 'django'
+        FLASK = 'flask'
+
+    @as_superset_of(PythonDependencies)
+    class Dependencies(SetEnum):
+        NGINX = 'nginx'
+        NODEJS = 'nodejs'
+    
+    return PythonDependencies, Dependencies
 
 
-@as_superset_of(PythonDependencies)
-class Dependencies(SetEnum):
-    NGINX = 'nginx'
-    NODEJS = 'nodejs'
+def create_via_subset():
+    class Dependencies(SetEnum):
+        NGINX = 'nginx'
+        NODEJS = 'nodejs'
 
+    @as_subset_of(Dependencies)
+    class PythonDependencies(SetEnum):
+        DJANGO = 'django'
+        FLASK = 'flask'
 
-print(isinstance(PythonDependencies.DJANGO, Dependencies))
+    return PythonDependencies, Dependencies
 
+    
+@pytest.fixture(params=['as_subset', 'as_superset'])
+def setenum_factory(request):
+    return create_via_subset if request.param == 'as_subset' else create_via_superset
+        
 
-def test_compare_to_superset():
+def test_compare_to_superset(setenum_factory):
+    PythonDependencies, Dependencies = setenum_factory()
+
     assert PythonDependencies.DJANGO in Dependencies
     assert PythonDependencies.DJANGO == Dependencies.DJANGO
     
 
-def test_lookup_superset_in_subset():
+def test_lookup_superset_in_subset(setenum_factory):
+    PythonDependencies, Dependencies = setenum_factory()
+    
     assert Dependencies.DJANGO in PythonDependencies
     assert Dependencies.NGINX not in PythonDependencies
 
 
-def test_is():
+def test_is_operator(setenum_factory):
+    PythonDependencies, Dependencies = setenum_factory()
+
     assert PythonDependencies.DJANGO is Dependencies.DJANGO
 
 
-def test_insinstance():
+def test_insinstance(setenum_factory):
+    PythonDependencies, Dependencies = setenum_factory()
+
     assert isinstance(PythonDependencies.DJANGO, Dependencies)
