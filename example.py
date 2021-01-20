4daed5663a579eb0086e8556cd426ea3f5e63ce7
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2021-01-21 01:55:47 +0200

    Working SetEnums, whoah

--- example.py
+++ example.py
@@ -8,64 +8,64 @@ class JavascriptDependencies(SetEnum):
     REACT = 'react'
 
 
+
 # This creates Dependencies.REACT value
 # Here subset is defined before bigger superset
 class Dependencies(SetEnum):
-    __subsets__ = (JavascriptDependencies, )
+    __subsets__ = [JavascriptDependencies, ]
     NGINX = 'nginx'
 
 
 # This creates Dependencies.FLASK and Dependencies.DJANGO
 # Here subset is defined after superset
 class PythonDependencies(SetEnum):
-    __supersets__ = (Dependencies, )
+    __supersets__ = [Dependencies, ]
     DJANGO = 'django'
     FLASK = 'flask'
 
 
 # Simple comparision validations
-# assert Dependencies.REACT is JavascriptDependencies.REACT
-a = Dependencies.FLASK
-# assert  is PythonDependencies.FLASK
+assert Dependencies.REACT is JavascriptDependencies.REACT
+assert Dependencies.FLASK is PythonDependencies.FLASK
 
-# assert Dependencies.REACT == JavascriptDependencies.REACT
-# assert Dependencies.FLASK == PythonDependencies.FLASK
+assert Dependencies.REACT == JavascriptDependencies.REACT
+assert Dependencies.FLASK == PythonDependencies.FLASK
 
-# # __in__ checks
-# assert Dependencies.FLASK in PythonDependencies
-# assert Dependencies.FLASK not in JavascriptDependencies
-# assert Dependencies.REACT in JavascriptDependencies
-# assert Dependencies.REACT not in PythonDependencies
+# __in__ checks
+assert Dependencies.FLASK in PythonDependencies
+assert Dependencies.FLASK not in JavascriptDependencies
+assert Dependencies.REACT in JavascriptDependencies
+assert Dependencies.REACT not in PythonDependencies
 
-# assert JavascriptDependencies.REACT in Dependencies
-# assert PythonDependencies.FLASK in Dependencies
+assert JavascriptDependencies.REACT in Dependencies
+assert PythonDependencies.FLASK in Dependencies
 
-# ### MYPY CHECKS, THAT SHOULD PASS ###
-# py_dep: Dependencies = PythonDependencies.FLASK
-# js_dep: Dependencies
-# js_dep = JavascriptDependencies.REACT
+### MYPY CHECKS, THAT SHOULD PASS ###
+py_dep: Dependencies = PythonDependencies.FLASK
+js_dep: Dependencies
+js_dep = JavascriptDependencies.REACT
 
-# # Just an other way to test the same
-# py_dep_temp = PythonDependencies.FLASK
-# dep_temp: Dependencies  = py_dep_temp
-
-
-# def foo(dep: Dependencies):
-#     pass
+# Just an other way to test the same
+py_dep_temp = PythonDependencies.FLASK
+dep_temp: Dependencies  = py_dep_temp
 
 
-# foo(Dependencies.NGINX)
-# foo(PythonDependencies.DJANGO)
-# foo(JavascriptDependencies.REACT)
+def foo(dep: Dependencies):
+    pass
 
 
-# ### MYPY CHECKS THAT HAVE TO FAIL
+foo(Dependencies.NGINX)
+foo(PythonDependencies.DJANGO)
+foo(JavascriptDependencies.REACT)
 
-# bad_dep: PythonDependencies
-# bad_dep = Dependencies.DJANGO  # just pass original ones, if needed
 
-# def foo_2(dep: PythonDependencies):
-#     pass
+### MYPY CHECKS THAT HAVE TO FAIL
 
-# foo_2(JavascriptDependencies.REACT)
-# foo_2(Dependencies.NGINX)
+bad_dep: PythonDependencies
+bad_dep = Dependencies.DJANGO  # just pass original ones, if needed
+
+def foo_2(dep: PythonDependencies):
+    pass
+
+foo_2(JavascriptDependencies.REACT)
+foo_2(Dependencies.FLASK)
--- setenum/mypy_plugin.py
+++ setenum/mypy_plugin.py
@@ -1,81 +1,99 @@
+from typing import Optional
 from mypy.nodes import (
     AssignmentStmt, Block, ClassDef, ListExpr,
-    NameExpr, SymbolTable, TypeInfo
+    NameExpr, SymbolTable, SymbolTableNode, TypeInfo
 )
 from mypy.plugin import ClassDefContext, Plugin
 
 
 
-def custom_get(self, name):
-    print('Access:', self.fullname)
+def lookup_in_subsets(typeinfo: TypeInfo, name: str):
+    subsets: Optional[SymbolTableNode] = typeinfo._subsets_
+    for subset in subsets.items:
+        subset_typeinfo = subset.node
+        res = subset_typeinfo.names.get(name) or lookup_in_subsets(subset_typeinfo, name)
+        if res:
+            return res
+
+
+def custom_get(self: TypeInfo, name: str):
     if name in ('__subsets__', '__supersets__'):
         return self.names.get(name)
 
     if name.startswith('__'):
         return TypeInfo.get(self, name)
     
-    for cls in self.mro:
-        n = cls.names.get(name)
-        if n:
-            return n
-    return None
+    if name in self.names:
+        return self.names.get(name)
+    else:
+        return lookup_in_subsets(self, name)
     
 
 class CustomPlugin(Plugin):
-    metadata = {}
     
-    def get_customize_class_mro_hook(self, fullname: str):
-        def analyze(classdef_ctx):
-            if not classdef_ctx.cls.base_type_exprs:
-                return
+    def get_base_class_hook(self, fullname: str):
+        if fullname == 'setenum.SetEnum':
+            return self.analyze
+    
+    # def get_customize_class_mro_hook(self, fullname: str):
+    #     # TODO: return analyze only for setenum ancestors or IDK
+    #     return analyze
 
-            base_cls_expr = classdef_ctx.cls.base_type_exprs[0]
-            if not isinstance(base_cls_expr, NameExpr):
-                return
-            
-            if not (
-                base_cls_expr.node
-                and isinstance(base_cls_expr.node, TypeInfo)
-                and base_cls_expr.node.defn.fullname == 'setenum.SetEnum'
-            ):
-                return
-            
-            
-            self.metadata[classdef_ctx.cls.fullname] = []
-            classdef_ctx.cls.info.get = custom_get.__get__(classdef_ctx.cls.info)
+    @classmethod
+    def analyze(cls, classdef_ctx):
+        if not classdef_ctx.cls.base_type_exprs:
+            return
 
-            subsets, supersets = ListExpr(items=[]), ListExpr(items=[])
-             
-            for assign_stmt in classdef_ctx.cls.defs.body:
-                # Multiple assignments are not allowed
-                if len(assign_stmt.lvalues) != 1:
-                    classdef_ctx.api.fail('Avoid using multiple assignment with SetEnum', assign_stmt)
-                    continue
+        base_cls_expr = classdef_ctx.cls.base_type_exprs[0]
+        if not isinstance(base_cls_expr, NameExpr):
+            return
+        
+        if not (
+            base_cls_expr.node
+            and isinstance(base_cls_expr.node, TypeInfo)
+            and base_cls_expr.node.defn.fullname == 'setenum.SetEnum'
+        ):
+            return
+        
+        print('### Analyze', classdef_ctx.cls.fullname)
+        classdef_ctx.cls.info.get = custom_get.__get__(classdef_ctx.cls.info)
+
+        # TODO: this is strange way to find subsets values
+        subsets, supersets = ListExpr(items=[]), ListExpr(items=[])
+            
+        for assign_stmt in classdef_ctx.cls.defs.body:
+            # Multiple assignments are not allowed
+            if len(assign_stmt.lvalues) != 1:
+                classdef_ctx.api.fail('Avoid using multiple assignment with SetEnum', assign_stmt)
+                continue
+            
+            lvalue = assign_stmt.lvalues[0]
+            if not isinstance(lvalue, NameExpr):
+                classdef_ctx.api.fail('Too hard to deduce for now', assign_stmt)
+                continue
+
+            if lvalue.name == '__subsets__':
+                subsets = assign_stmt.rvalue
+            elif lvalue.name == '__supersets__':
+                supersets = assign_stmt.rvalue
+        
+        # TODO: read about mypy caching, and how it can affect this
+        classdef_ctx.cls.info._subsets_ = subsets
+        
+        for subset_name in subsets.items:
+            res: TypeInfo = classdef_ctx.api.lookup_qualified(subset_name.name, classdef_ctx).node
+            res.mro.insert(1, classdef_ctx.cls.info)
+
+        for superset_name in supersets.items:
+            new_name_expr: NameExpr = NameExpr(classdef_ctx.cls.name)
+            new_name_expr.accept(classdef_ctx.api)
+            
+            res = classdef_ctx.api.lookup_qualified(superset_name.name, classdef_ctx).node
+            classdef_ctx.cls.info.mro.insert(1, res)
+
+            res._subsets_.items.append(new_name_expr)
                 
-                lvalue = assign_stmt.lvalues[0]
-                if not isinstance(lvalue, NameExpr):
-                    classdef_ctx.api.fail('Too hard to deduce for now', assign_stmt)
-                    continue
-
-                if lvalue.name == '__subsets__':
-                    subsets = assign_stmt.rvalue
-                elif lvalue.name == '__supersets__':
-                    supersets = assign_stmt.rvalue
-                else:
-                    self.metadata[classdef_ctx.cls.fullname].append(assign_stmt)
-
-            
-            for subset_name in subsets.items:
-                res: TypeInfo = classdef_ctx.api.lookup_qualified(subset_name.name, classdef_ctx).node
-                res.mro.insert(1, classdef_ctx.cls.info)
-
-
-            for superset_name in supersets.items:
-                res = classdef_ctx.api.lookup_qualified(superset_name.name, classdef_ctx).node
-                classdef_ctx.cls.info.mro.insert(1, res)
-                
-        # TODO: return analyze only for setenum ancestors or IDK
-        return analyze
+        
 
 def plugin(version: str):
     # no version restrictions yet
